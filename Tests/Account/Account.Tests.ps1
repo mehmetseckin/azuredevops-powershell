6b45839c829397038a058b0c87451a63858f6be0
Mehmet Seçkin <seckin92@gmail.com>
2018-10-23 10:56:20 +0100

    Add tests for account submodule

---
+++ Tests/Account/Account.Tests.ps1
@@ -0,0 +1,18 @@
+$ModuleName = "Account";
+Import-Module "$PSScriptRoot\..\..\AzureDevOps\Account" -Force;
+
+Describe 'Account submodule behaviour' {
+    $ModuleInfo = Get-Module -Name $ModuleName;
+    
+    It 'Exports a function named Add-AzureDevOpsAccount' {
+        $ModuleInfo.ExportedFunctions.ContainsKey('Add-AzureDevOpsAccount') | Should Be $True;
+    }
+
+    It 'Exports the alias with a name "Connect-AzureDevOpsAccount" and the definition "Add-AzureDevOpsAccount"' {
+        $ModuleInfo.ExportedAliases."Connect-AzureDevOpsAccount".Definition | Should Be 'Add-AzureDevOpsAccount';
+    }
+    
+    It 'Exports the alias with a name "Login-AzureDevOpsAccount" and the definition "Add-AzureDevOpsAccount"' {
+        $ModuleInfo.ExportedAliases."Login-AzureDevOpsAccount".Definition | Should Be 'Add-AzureDevOpsAccount';
+    }
+}
