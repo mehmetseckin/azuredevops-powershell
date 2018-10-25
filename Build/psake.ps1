238823586eb4452938cfa7d14e35b71b46dbfd81
Mehmet Seçkin <seckin92@gmail.com>
2018-10-25 16:25:52 +0100

    Simplify build code

--- Build/psake.ps1
+++ Build/psake.ps1
@@ -1,29 +1,17 @@
 # PSake makes variables declared here available in other scriptblocks
 Properties {
+
     # Find the build folder based on build system
     $ProjectRoot = $ENV:BHProjectPath
     if(-not $ProjectRoot)
     {
         $ProjectRoot = Resolve-Path "$PSScriptRoot\.."
     }
-
-    $Timestamp = Get-Date -UFormat "%Y%m%d-%H%M%S"
-    $PSVersion = $PSVersionTable.PSVersion.Major
-    $TestFile = "TestResults_PS$PSVersion`_$TimeStamp.xml"
-    $lines = '----------------------------------------------------------------------'
-
-    $Verbose = @{}
-    if($ENV:BHCommitMessage -match "!verbose")
-    {
-        $Verbose = @{Verbose = $True}
-    }
-    . "$PSScriptRoot\Update-CodeCoveragePercent.ps1";
 }
 
 Task Default -Depends Build
 
 Task Init {
-    $lines
     Set-Location $ProjectRoot
     "Build System Details:"
     Get-Item ENV:BH*
@@ -31,14 +19,17 @@ Task Init {
 }
 
 Task Test -Depends Init  {
-    $lines
-    "`n`tSTATUS: Testing with PowerShell $PSVersion"
+    "`n`tTesting with PowerShell $PSVersion"
+
+    $Timestamp = Get-Date -UFormat "%Y%m%d-%H%M%S"
+    $PSVersion = $PSVersionTable.PSVersion.Major
+    $TestFile = "TestResults_PS$PSVersion`_$TimeStamp.xml"
 
     # Gather test results. Store them in a variable and file
     $CodeFiles = (Get-ChildItem $ENV:BHModulePath -Recurse -Include "*.psm1","*.ps1").FullName
     $TestResults = Invoke-Pester -Path $ProjectRoot\Tests -PassThru -CodeCoverage $CodeFiles -OutputFormat NUnitXml -OutputFile "$ProjectRoot\$TestFile"
 
-    # TODO: Generate code coverage report
+    # TODO: Generate code coverage report (?)
     # $CoveragePercent = [math]::floor(100 - (($TestResults.CodeCoverage.NumberOfCommandsMissed / $TestResults.CodeCoverage.NumberOfCommandsAnalyzed) * 100))
 
     if($TestResults.FailedCount -gt 0)
@@ -49,8 +40,6 @@ Task Test -Depends Init  {
 }
 
 Task Build -Depends Test {
-    $lines
-    
     # Load the module, read the exported functions, update the psd1 FunctionsToExport
     Set-ModuleFunctions
 }
