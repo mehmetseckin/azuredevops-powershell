4b357c600ae5c2a6b650d899baa9afcae17f5b8e
Mehmet Seçkin <seckin92@gmail.com>
2018-10-26 16:52:13 +0100

    Add tests for Invoke-AzureDevOpsRestMethod

---
+++ Tests/Common/Public/Invoke-AzureDevOpsRestMethod.Tests.ps1
@@ -0,0 +1,34 @@
+Import-Module "$PSScriptRoot\..\..\..\AzureDevOps\Common" -Force;
+
+Describe "Invoke-AzureDevOpsRestMethod" {
+
+    Context 'Azure DevOps account was not added' {
+
+        BeforeAll {
+            Set-Variable -Name "AzureDevOpsAccount" -Scope Global -Value $null -Force;
+        }
+
+        It 'Throws an error message' {
+        
+            { Invoke-AzureDevOpsRestMethod } | 
+                Should -Throw;
+        }
+    }
+    
+    Context 'Azure DevOps account was added' {
+
+        BeforeAll {
+            Set-Variable -Name "AzureDevOpsAccount" -Scope Global -Value $true -Force;
+        }
+
+        Mock -ModuleName Common -CommandName Invoke-RestMethod { 
+            "Invoke-RestMethod called."
+        }
+
+        It 'Calls Invoke-RestMethod once' {
+        
+            Invoke-AzureDevOpsRestMethod
+            Assert-MockCalled -ModuleName Common -CommandName Invoke-RestMethod -Exactly -Times 1;
+        }
+    }
+}
